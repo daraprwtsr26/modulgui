ba1fb22d40495d508454accdc735ae8aac725a79
Daraprwtsr <Daraprwtsr@Daraprwtsr>
2017-09-29 11:32:09 +0700

    Modul5

---
+++ src/modulgui/MODULGUI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modulgui;
+
+/**
+ *
+ * @author Daraprwtsr
+ */
+public class MODULGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
